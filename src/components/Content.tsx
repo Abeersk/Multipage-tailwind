125a539a229654a60bbc40b5fa166440b8b2f6dd
Abeersk <Shayanshaikh4112@gmail.com>
2025-02-05 19:03:38 +0530

    Your update message

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -23,6 +23,7 @@ useEffect(()=>{
 <div className='pl-o md:pl-16 text-white text-center'>
 <h1 className='text-6xl font-bold drop-shadow-md'>Welcome To My_Shoes</h1>
 <p className='text-2xl mt-4'>Your One-Stop Shop For The Shoes</p>
+
 <button className='mt-6 px-6 py-3 bg-blue-700 rounded-md font-bold'>Shop Now</button>
 
 </div>
